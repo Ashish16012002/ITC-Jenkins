72e9133f0243467c7f1a3f306379e0c8e0044ea6
Ashish.Satapathy <ashish.satapathy@itcinfotech.com>
2024-07-25 06:23:39 +0000

    initial

---
+++ src/test/java/com/itc/jenkins/ItcJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.itc.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcJenkinsApplicationTests {
+	Logger logger = LoggerFactory.getLogger(ItcJenkinsApplication.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info("**********test case executed***********");
+		assertEquals(true, true);
+	}
+
+}
